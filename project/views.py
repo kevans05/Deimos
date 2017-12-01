6ec69f5caae08e9c691b3e2beea016ac9e33b493
Kyle Evans <k.evans@gmx.com>
2017-12-01 15:24:04 -0500

    almost back to phobos

--- project/views.py
+++ project/views.py
@@ -1,12 +1,27 @@
 from . import app
-from flask import render_template, request
+from flask import render_template, request, redirect
+
+import dataset
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html',
                            title='Home')
+@app.route('/newStaff')
+def newStaff():
+    return render_template('staffNew.html',
+                           title='about', page='about')
 
+@app.route('/handleNewStaff', methods=['POST'])
+def handleNewStaff():
+    db = dataset.connect('sqlite:///mydatabase.db')
+    table = db['CDMUsers']
+    table.insert(dict(firstName="xx"))
+    if request.method == 'POST':
+        print(request.form['inputFirstName'])
+       #'table.insert(dict(firstName=request.form['inputFirstName'], lastName=request.form['inputLastName'], email=request.form['inputEmail'], tel=request.form['inputPhoneNumber']))
+    return redirect('/')
 
 @app.route('/about')
 def about():
