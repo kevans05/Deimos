7ecae3f6fd91c16504f92d5b14fbe88f3e56997b
Kyle Evans <k.evans@gmx.com>
2017-12-01 14:04:38 -0500

    almost back to phobos

--- project/views.py
+++ project/views.py
@@ -6,3 +6,9 @@ from flask import render_template, request
 def index():
     return render_template('index.html',
                            title='Home')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html',
+                           title='about', page='about')
