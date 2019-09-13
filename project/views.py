4ecae08614abd3252bb90baaf7ad027386777982
KE server <evans.kyle5@gmail.com>
2019-09-13 12:09:26 -0400

    ok so its all there, i just need to move away from datasets and into sqlalachamy

--- project/views.py
+++ project/views.py
@@ -399,35 +399,38 @@ def about():
     return render_template('about.html',
                            title='about', page='about')
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
+    #if form.validate_on_submit():
+    if request.method == 'POST':
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', title='Sign In')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
-
+    #form = RegistrationForm()
+    #if form.validate_on_submit():
+    if request.method == 'POST':
+        user = User(firstName=request.form['inputFirstName'], email=request.form['inputEmail'])
+        print(request.form)
+        user.set_password(request.form['password'])
+        db = dataset.connect('sqlite:///project/dynamic/db/database.db')
+        table = db['staff']
+        table.append(user) 
+       #db.session.add(user)
+        #db.session.commit()
+        #flash('Congratulations, you are now a registered user!')
+        return redirect('login')
+    return render_template('register.html', title='Register')
 
 @app.route('/logout')
 def logout():
