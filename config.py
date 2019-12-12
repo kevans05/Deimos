582a978b764ccb03775243612dba86a5b9248b76
KE server <evans.kyle5@gmail.com>
2019-12-12 13:33:22 -0500

    fuck me we just need to do error handeling

--- config.py
+++ config.py
@@ -9,10 +9,10 @@ class Config(object):
                              'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'evans.j.k05@gmail.com'
+    MAIL_PASSWORD = 'hszlnafnuutobjin'
     ADMINS = ['your-email@example.com']
         
